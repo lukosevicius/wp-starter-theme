632c79beae2bb31b29012e42d96c9cb5f7245ed9
lukosevicius <man.lukosevicius@gmail.com>
2021-06-11 12:38:33 +0300

    updated helpers.php

--- includes/heleprs.php
+++ includes/heleprs.php
@@ -8,3 +8,26 @@ function get_current_archive_slug(){
 
     return get_queried_object()->slug;
 }
+
+function get_current_archive_top_slug(){
+
+    if ( ! is_archive() ){
+        return false;
+    };
+
+    $parent = get_queried_object()->parent;
+
+    if ( $parent ){
+        while ( $parent !== 0 ){
+            $parent_obj = get_term($parent);
+            $parent = get_term($parent)->parent;
+        };
+
+        return $parent_obj->slug;
+
+    } else {
+
+        return get_queried_object()->slug;
+
+    }
+}
