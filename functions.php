0e4f132be5c5122f17d8e03c53c7c49b4a16514b
lukosevicius <man.lukosevicius@gmail.com>
2021-07-13 10:28:46 +0300

    added inc/theme-support.php

--- functions.php
+++ functions.php
@@ -11,6 +11,7 @@ define('ML_DEV_MODE', true);
 *   INCLUDES
 */
 require_once 'inc/enqueue.php';
+require_once 'inc/theme-support.php';
 require_once 'inc/images.php';
 require_once 'inc/helpers.php';
 
