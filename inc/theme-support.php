a5694a5b02f86c66f1e7244e3a6870beeb53875c
lukosevicius <man.lukosevicius@gmail.com>
2021-07-13 10:55:56 +0300

    updated theme-support.php

--- inc/theme-support.php
+++ inc/theme-support.php
@@ -1,3 +1,11 @@
 <?php 
 
-add_theme_support( 'title-tag' );
+add_action( 'after_setup_theme', 'ml_theme_setup');
+
+
+function ml_theme_setup() {
+
+    add_theme_support( 'title-tag' );  
+    add_theme_support( 'html5', [ 'script', 'style' ] );
+
+}
